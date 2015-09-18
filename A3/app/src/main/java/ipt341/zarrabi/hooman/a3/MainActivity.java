f924780f6cc15ad22c863e225870d08c32789175
Hooman Zarrabi <hzarrabi@usc.edu>
2015-09-17 23:13:57 -0700

    fixed issue where italian button was and ImageView instead of an ImageButton.

--- A3/app/src/main/java/ipt341/zarrabi/hooman/a3/MainActivity.java
+++ A3/app/src/main/java/ipt341/zarrabi/hooman/a3/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("My new title");
+        initialize();
     }
 
     @Override
