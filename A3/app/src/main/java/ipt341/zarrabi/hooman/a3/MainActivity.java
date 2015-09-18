12690fa0cd2c56b719a7059ad6c1c516d7de5311
Hooman Zarrabi <hzarrabi@usc.edu>
2015-09-17 22:46:24 -0700

    changes to java

--- A3/app/src/main/java/ipt341/zarrabi/hooman/a3/MainActivity.java
+++ A3/app/src/main/java/ipt341/zarrabi/hooman/a3/MainActivity.java
@@ -1,11 +1,34 @@
 package ipt341.zarrabi.hooman.a3;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+
+    //integers for each button count
+    private int american;
+    private int chinese;
+    private int indian;
+    private int italian;
+    private int middleEast;
+    private int portugese;
+
+    //buttons
+    private ImageButton americanButton;
+    private ImageButton chineseButton;
+    private ImageButton indianButton;
+    private ImageButton italianButton;
+    private ImageButton middleEastButton;
+    private ImageButton portugeseButton;
+
+    //listner
+    View.OnClickListener buttonListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +56,63 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //initializes the buttons, listeners, and ints
+    private void initialize()
+    {
+        american=0;
+        indian=0;
+        chinese=0;
+        italian=0;
+        middleEast=0;
+        portugese=0;
+
+        americanButton = (ImageButton)findViewById(R.id.AmericanButton);
+        indianButton = (ImageButton)findViewById(R.id.IndianButton);
+        chineseButton= (ImageButton) findViewById(R.id.ChineseButton);
+        italianButton= (ImageButton) findViewById(R.id.ItalianButton);
+        middleEastButton= (ImageButton) findViewById(R.id.MiddleEastButton);
+        portugeseButton= (ImageButton) findViewById(R.id.PortugeseButton);
+
+        buttonListener= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(v.getId())
+                {
+                    case R.id.AmericanButton:
+                        american++;
+                        //TODO make toast
+                        break;
+
+                    case R.id.IndianButton:
+                        indian++;
+
+                        break;
+
+                    case R.id.ChineseButton:
+                        chinese++;
+
+                        break;
+
+                    case R.id.ItalianButton:
+                        italian++;
+
+                        break;
+
+                    case R.id.MiddleEastButton:
+                        middleEast++;
+
+                        break;
+
+                    case R.id.PortugeseButton:
+                        portugese++;
+
+                        break;
+
+                }
+
+            }
+        };
     }
 }
