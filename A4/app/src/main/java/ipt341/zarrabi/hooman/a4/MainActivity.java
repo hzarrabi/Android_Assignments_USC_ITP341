afe8ff3e962789cea78ab2efcf5c88c84a8da264
Hooman Zarrabi <hzarrabi@usc.edu>
2015-09-22 20:43:05 -0700

    edittext for bill amount now dynamic and doesn't rely on enter, tip amoung and total amound change with change in bill amount

--- A4/app/src/main/java/ipt341/zarrabi/hooman/a4/MainActivity.java
+++ A4/app/src/main/java/ipt341/zarrabi/hooman/a4/MainActivity.java
@@ -2,6 +2,8 @@ package ipt341.zarrabi.hooman.a4;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -15,6 +17,8 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class MainActivity extends Activity {
 
@@ -27,10 +31,10 @@ public class MainActivity extends Activity {
     private TextView perPersontText;
     private TextView amountPerPerson;
 
-    BigDecimal billAmount;
-    BigDecimal tax;
-    BigDecimal tip;
-    BigDecimal total;
+    double billAmount;
+    double tax;
+    double tip;
+    double total;
 
 
     @Override
@@ -77,10 +81,10 @@ public class MainActivity extends Activity {
         perPersontText = (TextView) findViewById(R.id.PerPersonText);
         amountPerPerson = (TextView) findViewById(R.id.PerPerson);
 
-        billAmount= new BigDecimal("0.00");
-        tax=new BigDecimal("0.00");
-        tip=new BigDecimal("0.00");
-        total=new BigDecimal("0.00");
+        billAmount=0.00;
+        tax=0.00;
+        tip=0.00;
+        total=0.00;
 
 
         //the following two should inititally  be invisible
@@ -91,24 +95,52 @@ public class MainActivity extends Activity {
     private void setListeners()
     {
 
-        amount.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        amount.addTextChangedListener(new TextWatcher() {
             @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                billAmount=new BigDecimal(amount.getText().toString());//changing the bill amount
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                return false;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (amount.getText().toString().trim().length() == 0) billAmount=0.00;//in case the text edit is empty
+
+                else billAmount = Double.parseDouble(amount.getText().toString());//changing the bill amount
+
+                percentText.setText(Integer.toString(percentBar.getProgress()) + "%");
+                tax = percentBar.getProgress() / 100.00;
+
+                tip = billAmount * tax;
+                tipAmount.setText(NumberFormat.getCurrencyInstance().format(tip));
+
+                total = tip + billAmount;
+                totalAmount.setText(NumberFormat.getCurrencyInstance().format(total));
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
+
 
         percentBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                percentText.setText(Integer.toString(progress)+"%");
-                tax= new BigDecimal(progress/(100.00));
-                Log.d("checking percent", "tax is" + tax.doubleValue());
+                percentText.setText(Integer.toString(progress) + "%");
+                tax = progress / 100.00;
+                Log.d("checking percent", "tax is" + tax);
 
-                //tip=new BigDecimal(billAmount.)
+                tip = billAmount * tax;
+                Log.d("checking percent", "tip is" + tip);
+                tipAmount.setText(NumberFormat.getCurrencyInstance().format(tip));
+
+                total = tip + billAmount;
+                Log.d("checking percent", "total is" + tip);
+                totalAmount.setText(NumberFormat.getCurrencyInstance().format(total));
+
 
             }
 
