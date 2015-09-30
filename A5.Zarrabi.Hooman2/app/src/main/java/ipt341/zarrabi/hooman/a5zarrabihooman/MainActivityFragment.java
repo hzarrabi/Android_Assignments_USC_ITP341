84bcb66e816428e1f2f4fbbebb08c4753b1c83aa
Hooman Zarrabi <hzarrabi@usc.edu>
2015-09-30 14:49:58 -0700

    initial commit of assignment 5 - fragments

---
+++ A5.Zarrabi.Hooman2/app/src/main/java/ipt341/zarrabi/hooman/a5zarrabihooman/MainActivityFragment.java
@@ -0,0 +1,22 @@
+package ipt341.zarrabi.hooman.a5zarrabihooman;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
